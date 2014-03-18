7ba8b85d13d7b3b9480ac8c85d29b85528143530
ErosZy <395693101@qq.com>
2014-03-18 12:12:29 +0800

    by zhaoyang

--- UME.js
+++ UME.js
@@ -9,37 +9,43 @@
  * 2.内联使用use的依赖正则解析
  */
 ;var UME = (function(w, u) {
-	var window = w,
-		undefined = u,
-		UME = {};
-		
-	
-	var _moduleCache = {},
-        _moduleQueue = [],
+    var window = w,
+        undefined = u,
+        UME = {};
+
+
+    var _moduleCache = {},
+        _loading = [],
         _all = [];
 
     /**
      * 定义模块变量
      */
-	UME.define = function() {
-		var self = this,
-			argsLen = arguments.length,
-            i = 0,
-            fn,modules,modulesInfo,len,path;
+    UME.define = function() {
+        var self = this,
+            argsLen = arguments.length,
+            count = 0,
+            fn,modules,modulesInfo,len,path,i;
 
-		//说明没有传入requires参数
-		if (argsLen <= 2) {
-			modules = [];
-			fn = self._isFunction(arguments[1]) ? arguments[1] : function(){};
-		} else {
-			modules = self._isArray(arguments[1]) ? arguments[1] : [];
-			fn = self._isFunction(arguments[2]) ? arguments[2] : function(){};
-		}
+        //说明没有传入requires参数
+        if (argsLen <= 2) {
+            modules = [];
+            fn = self._isFunction(arguments[1]) ? arguments[1] : function(){};
+        } else {
+            modules = self._isArray(arguments[1]) ? arguments[1] : [];
+            fn = self._isFunction(arguments[2]) ? arguments[2] : function(){};
+        }
 
-        path = arguments[0];
+        //转换路径，统一为绝对路径
+        path = self._toPath(arguments[0]);
+        for(i = 0,len = modules.length; i < len; i++){
+            modules[i] = self._toPath(modules[i]);
+            _loading.push(modules[i]);
+        }
 
+        //获取需要加载的模块
         modulesInfo = self._getRequireModulesInfo(modules);
-        len = modulesInfo.requires.length;
+        len = modulesInfo.length;
 
         /*
          * 如果len为0则说明：
@@ -47,6 +53,7 @@
          * 2.没有所依赖的模块,只传入了模块定义
          */
         if(!len){
+            //debugger;
             var params = self._getModulesInstance(modules),
                 obj = fn.apply(self,params);
 
@@ -56,28 +63,27 @@
             self._emitAll();
 
         }else{
-            //否则需要加载这些模块
-
             _all.push(all);
 
-            for(var i = 0; i < len; i++){
-                self._load(modulesInfo.requires[i],function(){
+            //否则需要加载这些模块
+            for(i = 0; i < len; i++){
+                self._load(modulesInfo[i],function(){
                     bind();
                     self._emitAll();
                 })
             }
         }
 
-		function bind() {
-			i++;
-		}
+        function bind() {
+            count++;
+        }
 
         function all(){
             var len = modules.length,
                 params,obj;
 
             //保证依赖模块是加载完成的
-            if(i < len)
+            if(count < len)
                 return false;
 
             params = self._getModulesInstance(modules);
@@ -92,43 +98,37 @@
 
             return true;
         }
-	}
+    }
 
     /**
      * 外部调用模块化
      * @param path
      * @param callback
      */
-	UME.use = function(path, callback) {
+    UME.use = function(path, callback) {
 
-	}
+    }
 
     /**
      * 获取模块信息，包括缓存的模块和需要加载的模块
      * @param arr
-     * @returns {{requires: Array, caches: Object}}
+     * @returns {requires:Array}
      */
     UME._getRequireModulesInfo = function(arr){
         var self = this,
             requires = [],
-            caches = {},
             i,len;
 
         for(i = 0, len = arr.length; i < len; i++){
             var module = arr[i],
-                isCached = module in _moduleCache && _moduleCache[module];
+                isCached = module in _moduleCache  && self._getLoadingIndex(module) != -1;
 
-            if(isCached){
-                caches[module] = _moduleCache[module];
-            }else{
+            if(!isCached){
                 requires.push(module);
             }
         }
 
-        return {
-            requires : requires,
-            caches : caches
-        }
+        return requires;
 
     }
 
@@ -167,119 +167,152 @@
         }
     }
 
+    UME._getLoadingIndex = function(module){
+        var self = this,
+            index = -1;
 
-	/**
-	 * 事件绑定函数
-	 * @param ele 绑定对象
-	 * @param eventName 事件名
-	 * @param fn 事件函数
-	 * @private
-	 */
-	UME._on = function(ele, eventName, fn) {
-		var self = this,
-			event = eventName + fn.toString().replace(/\s+/g, '');
+        for(var i = 0, len = _loading.length; i < len; i++){
+            if(_loading[i] == module)
+                index = i
+        }
 
-		ele[eventName] = ele[eventName] ? ele[eventName] : {};
-		ele[eventName][event] = fn;
+        return index;
+    }
 
-		if (document.attachEvent) {
-			ele.attachEvent("on" + eventName, function(ev) {
-				fn.call(ele, ev);
-			});
-		} else if (document.addEventListener) {
-			ele.addEventListener(eventName, fn, false);
-		} else {
-			ele["on" + eventName] = ele.eventName[event];
-		}
-	}
 
-	/**
-	 * 事件取消函数
-	 * @param ele 取消对象
-	 * @param eventName 事件名
-	 * @param fn 事件函数
-	 * @private
-	 */
-	UME._off = function(ele, eventName, fn) {
-		var self = this,
-			event;
+    /**
+     * 事件绑定函数
+     * @param ele 绑定对象
+     * @param eventName 事件名
+     * @param fn 事件函数
+     * @private
+     */
+    UME._on = function(ele, eventName, fn) {
+        var self = this,
+            event = eventName + fn.toString().replace(/\s+/g, '');
 
-		//如果小于3，就删除所有事件
-		//否则就删除对应的事件
-		if (arguments.length < 3) {
-			if (eventName) {
-				if (document.detachEvent) {
-					for (var item in ele[eventName]) {
-						if (ele[eventName].hasOwnProperty(item))
-							ele.detachEvent("on" + eventName, ele[eventName][item]);
-					}
-				} else if (document.removeEventListener) {
-					for (var item in ele[eventName]) {
-						if (ele[eventName].hasOwnProperty(item))
-							ele.removeEventListener(eventName, ele[eventName][item]);
-					}
-				} else {
-					ele["on" + eventName] = null;
-				}
-				ele[eventName] = null;
-			} else {
-				return;
-			}
-		} else {
-			event = eventName + fn.toString().replace(/\s+/g, '');
-			fn = ele[eventName][event];
+        ele[eventName] = ele[eventName] ? ele[eventName] : {};
+        ele[eventName][event] = fn;
 
-			if (typeof fn != "function") return;
+        if (document.attachEvent) {
+            ele.attachEvent("on" + eventName, function(ev) {
+                fn.call(ele, ev);
+            });
+        } else if (document.addEventListener) {
+            ele.addEventListener(eventName, fn, false);
+        } else {
+            ele["on" + eventName] = ele.eventName[event];
+        }
+    }
 
-			if (document.detachEvent) {
-				ele.detachEvent("on" + eventName, function(ev) {
-					fn.call(ele, ev);
-				});
-			} else if (document.removeEventListener) {
-				ele.removeEventListener(eventName, fn, false);
-			} else {
-				ele[eventName][event] = null;
-			}
-		}
-	}
+    /**
+     * 事件取消函数
+     * @param ele 取消对象
+     * @param eventName 事件名
+     * @param fn 事件函数
+     * @private
+     */
+    UME._off = function(ele, eventName, fn) {
+        var self = this,
+            event;
 
-	/**
-	 * 加载script
-	 * @param path 文件路径
-	 * @param fn 回调函数
-	 * @private
-	 */
-	UME._load = function(path, fn) {
-		var self = this,
-			isIE = document.attachEvent ? true : false,
-			body = document.getElementsByTagName("body")[0],
-            script;
+        //如果小于3，就删除所有事件
+        //否则就删除对应的事件
+        if (arguments.length < 3) {
+            if (eventName) {
+                if (document.detachEvent) {
+                    for (var item in ele[eventName]) {
+                        if (ele[eventName].hasOwnProperty(item))
+                            ele.detachEvent("on" + eventName, ele[eventName][item]);
+                    }
+                } else if (document.removeEventListener) {
+                    for (var item in ele[eventName]) {
+                        if (ele[eventName].hasOwnProperty(item))
+                            ele.removeEventListener(eventName, ele[eventName][item]);
+                    }
+                } else {
+                    ele["on" + eventName] = null;
+                }
+                ele[eventName] = null;
+            } else {
+                return;
+            }
+        } else {
+            event = eventName + fn.toString().replace(/\s+/g, '');
+            fn = ele[eventName][event];
 
-		script = document.createElement("script");
-		script.src = path;
+            if (typeof fn != "function") return;
 
-		if (isIE) {
-			self._on(script, "readystatechange", function() {
-				if (script.readyState == "complete")
+            if (document.detachEvent) {
+                ele.detachEvent("on" + eventName, function(ev) {
+                    fn.call(ele, ev);
+                });
+            } else if (document.removeEventListener) {
+                ele.removeEventListener(eventName, fn, false);
+            } else {
+                ele[eventName][event] = null;
+            }
+        }
+    }
+
+    /**
+     * 加载script
+     * @param path 文件路径
+     * @param fn 回调函数
+     * @private
+     */
+    UME._load = function(path, fn) {
+        var self = this,
+            isIE = document.attachEvent ? true : false,
+            body = document.getElementsByTagName("body")[0],
+            script,index;
+
+        script = document.createElement("script");
+        script.src = path;
+        index = self._getLoadingIndex(path);
+
+        if (isIE) {
+            self._on(script, "readystatechange", function() {
+                if (script.readyState == "complete"){
+                    if(index != -1)
+                        _loading.splice(index,1);
+
                     fn();
-			})
-		} else {
-			self._on(script, "load", function() {
-				fn();
-			})
-		}
+                }
+            })
+        } else {
+            self._on(script, "load", function() {
+                if(index != -1)
+                    _loading.splice(index,1);
 
-		body.appendChild(script);
+                fn();
+            })
+        }
 
-	}
+        body.appendChild(script);
+
+    }
 
     /**
      * 路径转换
      * @private
      */
-	UME._toPath = function() {
+    UME._toPath = function(path) {
+        var self = this,
+            location = window.location,
+            origin = location.origin,
+            pathname = location.pathname,
+            reg = /^(\/[^\.]+\/)/i,
+            url;
 
-	}
+        if(reg.test(pathname)){
+            url = origin + RegExp["$1"] + path;
+        }else{
+            url = origin + path;
+        }
+
+        return url
+    }
 
     /**
      * 判断是否是函数
@@ -312,6 +345,6 @@
     }
 
 
-	return UME;
+    return UME;
 
 }(window, undefined));
